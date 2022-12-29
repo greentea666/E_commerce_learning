7f3b5fda8469fdba6ba70b176df919b3e815d911
ArthurLin <t410002246@gmail.com>
2022-12-29 23:21:01 +0800

    add swagger autogen doc and test api.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,27 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
+import swaggerJsDoc from"swagger-jsdoc";
+import swaggerUi  from"swagger-ui-express";
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Customer API",
+      description: "Customer API Information",
+      contact: {
+        name: "Amazing Developer"
+      },
+      servers: ["http://localhost:5000"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ["server.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 dotenv.config();
 
 mongoose
@@ -19,13 +40,36 @@ mongoose
   });
 
 const app = express();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+// Routes
+/**
+ * @swagger
+ * /customers:
+ *  get:
+ *    description: gernerate data for frontend view
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+// Routes
+/**
+ * @swagger
+ * /order:
+ *  get:
+ *    description: gernerate data for frontend view
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
